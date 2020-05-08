7097452a2eb4d504f0c670c11d23c351fbaba0fa
amiddleb <60206926+amiddleb@users.noreply.github.com>
2020-05-08 16:36:13 -0400

    m

--- Project5/js/main.js
+++ Project5/js/main.js
@@ -1,14 +1,14 @@
 $(document).ready(function(){
-  $(".curtaintop").click(function() {
+  $(".tassle").click(function() {
     $(".curtain").toggleClass("curtainUp");
   });
-  $(".spotlight").click(function(){
+  $(".lamp").click(function(){
     $(".spotlight").toggleClass("lightSwitch");
   });
-  $(".spotlight2").click(function(){
+  $(".lamp2").click(function(){
     $(".spotlight2").toggleClass("lightSwitch");
   });
-  $(".spotlight3").click(function(){
+  $(".lamp3").click(function(){
     $(".spotlight3").toggleClass("lightSwitch");
   });
 });
